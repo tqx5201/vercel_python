5dedc3f90c09d3cdc531ee167ac71ca893914274
tqx5201 <tqx5201@163.com>
2023-07-05 15:53:50 +0800

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 DEBUG = True
 
 #ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','.stncp.top']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 # Application definition
 
